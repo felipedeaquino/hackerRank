a7b6ee133a3faa1e6dc0d329182a818e43fe4943
felipedeaquino <felipe.puziol@gmail.com>
2021-09-22 18:29:38 -0300

    Create day1_data_types.js

---
+++ day1_data_types.js
@@ -0,0 +1,19 @@
+
+    // Declare second integer, double, and String variables.
+    var outroI;
+    var outroD;
+    var outroS;
+    // Read and save an integer, double, and String to your variables.
+    outroI = parseInt(readLine());
+    outroD = parseFloat(readLine());
+    outroS = readLine();
+    // Print the sum of both integer variables on a new line.
+    var somaIs = i + outroI;
+    console.log(somaIs);
+    // Print the sum of the double variables on a new line.
+    var somaDs = d + outroD;
+    console.log(somaDs.toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    var somaSs = s + outroS;
+    console.log(somaSs);
