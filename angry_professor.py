8d975ab4208c01a7e1e6fd209f742c589e10ef11
felipedeaquino <felipe.puziol@gmail.com>
2021-09-29 06:48:43 -0300

    Create angry_professor.py

---
+++ angry_professor.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a):
+    students = len(a)
+    counter = 0
+#    answer = []
+    for i in range(students):
+        if a[i] <= 0:
+            counter += 1
+    if counter >= k:
+#        answer.append("YES")
+        return "NO"
+    else:
+        return "YES"
+#        answer.append("NO")
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
