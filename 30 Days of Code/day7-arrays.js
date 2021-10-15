c019a879f6c515c43f34761dbea0b5322adca05a
felipedeaquino <felipe.puziol@gmail.com>
2021-10-14 21:33:02 -0300

    Create day7-arrays.js

---
+++ 30 Days of Code/day7-arrays.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+//my function:
+function response(n, arr) {
+    let reversedArr = arr.reverse();
+    reversedArr = reversedArr.join(' ');
+    console.log(reversedArr);
+}
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    response(n, arr);
+}
