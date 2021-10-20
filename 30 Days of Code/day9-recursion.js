bbadb8ba8ad1df8939de16060a7a5c46ec8fcc4a
felipedeaquino <felipe.puziol@gmail.com>
2021-10-20 10:07:47 -0300

    20-10-2021

---
+++ 30 Days of Code/day9-recursion.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function factorial(n) {
+    if (n <= 0) {
+      return 1;
+    } else {
+      return factorial(n - 1) * n;
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = factorial(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
